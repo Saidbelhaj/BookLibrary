4f51937a762496c1851cd85ac422aae8fa67d3f4
Said Bel Haj Letaief <93030935+Saidbelhaj@users.noreply.github.com>
2022-05-14 05:22:14 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyCbBkrYtVUEN72wmwhyAhX3R0So6EzPt58",
+    apiKey: "*****************************",
     authDomain: "books-2ca61.firebaseapp.com",
     databaseURL: "https://books-2ca61-default-rtdb.firebaseio.com",
     projectId: "books-2ca61",
     storageBucket: "books-2ca61.appspot.com",
     messagingSenderId: "447193046625",
-    appId: "1:447193046625:web:52c331c7e50e0835b29420",
+    appId: "*******************************",
     measurementId: "G-3YEVVGM61L",
   },
 };
